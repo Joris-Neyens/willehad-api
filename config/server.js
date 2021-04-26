24c85e9dee5030200ea9d4c523aa8ddfa42b35bd
Joris-Neyens <57662441+Joris-Neyens@users.noreply.github.com>
2021-04-26 13:47:34 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b882d988273aa283f75fe18c4af67456'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '649d1c26-c057-47ae-8e68-a5df7cebedd8'
+};
